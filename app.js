7b3d62aa89d87f72e77c35424a3e64c1adb94321
yurii hlukhyi <yhlukhyi@gmail.com>
2022-12-09 15:25:04 -0800

    html done

---
+++ app.js
@@ -0,0 +1,8 @@
+// Check
+document.getElementById("checkbox").checked = true;
+
+// Uncheck
+document.getElementById("checkbox").checked = false;
+
+<form action="/recipe-form.js">
+  <input type="checkbox"  id="recipe"  name="milk"  value="1 1/2 cups milk"
